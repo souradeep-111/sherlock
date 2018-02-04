61a8d99786d0b0ba2b17ab362639417e0e89e5b2
Souradeep Dutta <souradeep.dutta@colorado.edu>
2018-02-04 11:18:48 -0700

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,40 +1,21 @@
-CFLAG_1 := -w
-CLFAG_2 := -lgurobi_c++
-CFLAG_3 := -lgurobi75
-CFLAG_4 := -std=c++11
-CFLAG_5 := -lm
-CFLAG_6 := -D_GLIBCXX_USE_CXX11_ABI=0
-CFLAG_7 := -lglpk
+CXX = g++
+LIBS = -lgurobi_c++ -lgurobi75 -lm -D_GLIBCXX_USE_CXX11_ABI=0 -m64 -w
+CFLAGS = -I . -I ./headers -I /usr/local/include/ -I /opt/gurobi751/linux64/include/ -g -O3 -std=c++11
+LINK_FLAGS = -g -L ./ -L /usr/local/lib/ -L /opt/gurobi751/linux64/lib/
 
-LFLAG_1 := -L /usr/local/lib/
-LFLAG_2 := -L ./
-LFLAG_3 := -L /opt/gurobi751/linux64/lib/
+OBJS = ./headers/propagate_intervals.o ./headers/network_computation.o ./headers/gurobi_interface.o ./headers/configuration.o
 
-IFLAG_1 := -I ./headers
-IFLAG_2 := -I /usr/local/include/
-IFLAG_3 := -I /opt/gurobi751/linux64/include/
 
-headers_path := ./headers/
+run_file: main.o $(OBJS)
+	g++ -O3 -w $(LINK_FLAGS) -o $@ $^ $(LIBS)
 
-run_file: main.o propagate_intervals.o network_computation.o \
-  gurobi_interface.o
-	g++  $(headers_path)propagate_intervals.o $(headers_path)network_computation.o \
-  $(headers_path)gurobi_interface.o $(headers_path)main.o\
-	 -o run_file $(LFLAG_3) -lgurobi_c++ $(CFLAG_3) $(CFLAG_4) \
-	 $(CFLAG_5) $(CFLAG_6)
 
-main.o: main.cpp
-	g++ -o $(headers_path)main.o -c main.cpp $(IFLAG_2) $(IFLAG_3) $(CFLAG_4)
-
-propagate_intervals.o: $(headers_path)propagate_intervals.cpp $(headers_path)propagate_intervals.h
-	g++ -o $(headers_path)propagate_intervals.o -c $(headers_path)propagate_intervals.cpp $(IFLAG_2) $(IFLAG_3) $(CFLAG_4)
-
-network_computation.o: $(headers_path)network_computation.cpp $(headers_path)network_computation.h
-	g++ -o $(headers_path)network_computation.o -c $(headers_path)network_computation.cpp $(IFLAG_3) $(CFLAG_4)
-
-gurobi_interface.o: $(headers_path)gurobi_interface.cpp $(headers_path)gurobi_interface.h
-	g++ -m64 -g -o $(headers_path)gurobi_interface.o -c $(headers_path)gurobi_interface.cpp \
-	$(IFLAG_3) $(CFLAG_6)
+%.o: %.cc
+	$(CXX) -O3 -c $(CFLAGS) $(LIBS) -o $@ $<
+%.o: %.cpp
+	$(CXX) -O3 -c $(CFLAGS) $(LIBS) -o $@ $<
+%.o: %.c
+	$(CXX) -O3 -c $(CFLAGS) $(LIBS) -o $@ $<
 
 clean:
-	rm -f run_file ./headers/main.o ./headers/propagate_intervals.o ./headers/network_computation.o ./headers/gurobi_interface.o
+	rm -f ./headers/*.o *.o
--- main.cpp
+++ main.cpp
@@ -1,97 +1,360 @@
 #include "./headers/propagate_intervals.h"
 using namespace std;
 using namespace std::chrono;
-datatype network_offset = -100;
-datatype offset_in_constraint_comb = constr_comb_offset;
 
 int main()
 {
 	// Simple range propagation
-	char controller_file[] = "./network_files/neural_network_information" ;
-	network_handler system_network(controller_file);
-
+	sherlock_parameters.verbosity = true;
+	sherlock_parameters.grad_search_point_verbosity = true;
+	sherlock_parameters.time_verbosity = true;
 	vector< vector< datatype > > input_interval(2, vector< datatype >(2,0));
+	vector< vector< datatype > > input_constraints;
+	clock_t begin, end;
+	vector< datatype > output_range(2,0);
+
+	char benchmark_0_name[] = "./network_files/neural_network_information_0" ;
+	network_handler benchmark_0(benchmark_0_name);
+
 	input_interval[0][0] = 0;input_interval[0][1] = 10;
 	input_interval[1][0] = 0;input_interval[1][1] = 10;
 
-	vector< vector< datatype > > input_constraints;
 	create_constraint_from_interval(input_constraints, input_interval);
 
-	clock_t begin, end;
 	begin = clock();
 
-	vector< datatype > output_range(2,0);
 
- 	system_network.return_interval_output(input_constraints, output_range, 1);
-	//
+ 	benchmark_0.return_interval_output(input_constraints, output_range, 1);
 	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
-
-
 	end = clock();
-	printf("time cost for Sherlock ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+	printf("time cost for Sherlock benchmark 0 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+
+	// Simple range propagation
+	char benchmark_1_name[] = "./network_files/neural_network_information_1" ;
+	sherlock_parameters.MILP_tolerance = 1e-2;
+	network_handler benchmark_1(benchmark_1_name);
+
+	input_interval[0][0] = 0;input_interval[0][1] = 10;
+	input_interval[1][0] = 0;input_interval[1][1] = 10;
+
+	create_constraint_from_interval(input_constraints, input_interval);
 
 	begin = clock();
-
-	vector< vector< vector< datatype > > > weights;
-	vector< vector< datatype > > biases;
-
-	system_network.return_network_information(weights, biases);
-	datatype milp_ret;
-	vector< datatype > counter_example;
-	datatype upper_lim;
-	int direction = -1;
-	milp_ret = do_MILP_optimization(input_constraints, weights, biases, counter_example, direction);
-	cout << "Min found = " << milp_ret << endl;
-
-	direction = 1;
-	milp_ret = do_MILP_optimization(input_constraints, weights, biases, counter_example, direction);
-	cout << "Max found = " << milp_ret << endl;
-
+	benchmark_1.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
 	end = clock();
-	printf("time cost for Monolithic MILP ---------------------- %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+	printf("time cost for Sherlock benchmark 1 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	// Simple range propagation
+	char benchmark_2_name[] = "./network_files/neural_network_information_2" ;
+	sherlock_parameters.gradient_rate = 1e-4;
+	sherlock_parameters.grad_scaling_factor = 2e1;
+	sherlock_parameters.MILP_tolerance = 8e-2;
+	sherlock_parameters.scale_factor_for_M = 1.3;
+
+	network_handler benchmark_2(benchmark_2_name);
+
+	input_interval[0][0] = 0;input_interval[0][1] = 10;
+	input_interval[1][0] = 0;input_interval[1][1] = 10;
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_2.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 2 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
 
 
-	// clock_t begin, end;
-	// begin = clock();
-	// find_limits_using_reluplex(input_interval, output_range);
-	// end = clock();
-	// printf("time cost for reluplex : %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
-	// cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	// Simple range propagation
+	char benchmark_3_name[] = "./network_files/neural_network_information_3" ;
 
-	// Just verifying the limits using Reluplex
-	steady_clock::time_point t1 = steady_clock::now();
+	network_handler benchmark_3(benchmark_3_name);
 
-	vector< datatype > limits(2);
-	limits[0] = -1e10;
-	limits[1] = output_range[0] - MILP_tolerance;
+	input_interval[0][0] = 0;input_interval[0][1] = 10;
+	input_interval[1][0] = 0;input_interval[1][1] = 10;
 
-	int check = check_limits_using_reluplex(limits, input_interval, -1, counter_example);
-	if(!check)
-	{
-		cout << "lower limits verified .. " << endl;
-	}
-	else
-	{
-		cout << "lower limits false .. " << endl;
-	}
+	create_constraint_from_interval(input_constraints, input_interval);
 
-	limits[0] = output_range[1] + MILP_tolerance;
-	limits[1] = 1e10;
+	begin = clock();
+	benchmark_3.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 3 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
 
-	check = check_limits_using_reluplex(limits, input_interval, 1, counter_example);
-	if(!check)
-	{
-		cout << "upper limits verified .. " << endl;
-	}
-	else
-	{
-		cout << "Upper limits false .. " << endl;
-	}
 
-	steady_clock::time_point t2 = steady_clock::now();
-	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-	cout << "Reluplex time =  " << time_span.count() << " seconds." << endl;
+	// Simple range propagation
+	char benchmark_4_name[] = "./network_files/neural_network_information_4" ;
 
+	network_handler benchmark_4(benchmark_4_name);
+
+	input_interval[0][0] = 0;input_interval[0][1] = 10;
+	input_interval[1][0] = 0;input_interval[1][1] = 10;
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_4.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 4 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	// Simple range propagation
+	char benchmark_5_name[] = "./network_files/neural_network_information_5" ;
+	sherlock_parameters.MILP_tolerance = 2e-2;
+
+	network_handler benchmark_5(benchmark_5_name);
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.5);input_interval[0].push_back(0.5);;
+	input_interval[1].push_back(-0.5);input_interval[1].push_back(0.5);;
+	input_interval[2].push_back(-0.5);input_interval[2].push_back(0.5);;
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_5.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 5 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	Simple range propagation
+	char benchmark_6_name[] = "./network_files/neural_network_information_6" ;
+
+	network_handler benchmark_6(benchmark_6_name);
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.5);input_interval[0].push_back(0.5);;
+	input_interval[1].push_back(-0.5);input_interval[1].push_back(0.5);;
+	input_interval[2].push_back(-0.5);input_interval[2].push_back(0.5);;
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_6.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 6 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	Simple range propagation
+	char benchmark_7_name[] = "./network_files/neural_network_information_7" ;
+	network_handler benchmark_7(benchmark_7_name);
+
+	input_interval.clear();
+	input_interval.resize(4);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+	input_interval[3].push_back(-0.1);input_interval[3].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_7.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 7 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+
+	char benchmark_8_name[] = "./network_files/neural_network_information_8" ;
+	network_handler benchmark_8(benchmark_8_name);
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.5);input_interval[0].push_back(0.5);
+	input_interval[1].push_back(-0.5);input_interval[1].push_back(0.5);
+	input_interval[2].push_back(-0.5);input_interval[2].push_back(0.5);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_8.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 8 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_9_name[] = "./network_files/neural_network_information_9" ;
+	network_handler benchmark_9(benchmark_9_name);
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.5);input_interval[0].push_back(0.5);
+	input_interval[1].push_back(-0.5);input_interval[1].push_back(0.5);
+	input_interval[2].push_back(-0.5);input_interval[2].push_back(0.5);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_9.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 9 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_10_name[] = "./network_files/neural_network_information_10" ;
+	sherlock_parameters.gradient_rate = 1e-6;
+	sherlock_parameters.grad_scaling_factor = 2e1;
+	sherlock_parameters.grad_switch_count = 1e2;
+	sherlock_parameters.grad_termination_limit = 1e-6;
+	sherlock_parameters.MILP_e_tolerance = 1e-15;
+	sherlock_parameters.scale_factor_for_M = 1.5;
+	sherlock_parameters.MILP_tolerance = 5e-2;
+
+	network_handler benchmark_10(benchmark_10_name);
+
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_10.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 10 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_11_name[] = "./network_files/neural_network_information_11" ;
+	sherlock_parameters.gradient_rate = 1e-4;
+	sherlock_parameters.grad_scaling_factor = 5e1;
+	sherlock_parameters.grad_switch_count = 1e3;
+	sherlock_parameters.grad_termination_limit = 1e-7;
+	sherlock_parameters.MILP_tolerance = 5e-1;
+	sherlock_parameters.MILP_e_tolerance = 1e-10;
+	sherlock_parameters.scale_factor_for_M = 1;
+	sherlock_parameters.do_LP_certificate = true;
+	sherlock_parameters.LP_tolerance_limit = 1e-2;
+
+	network_handler benchmark_11(benchmark_11_name);
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_11.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 11 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_12_name[] = "./network_files/neural_network_information_12" ;
+	network_handler benchmark_12(benchmark_12_name);
+	sherlock_parameters.gradient_rate = 1e-2;
+	sherlock_parameters.grad_scaling_factor = 5e1;
+	sherlock_parameters.grad_switch_count = 1e3;
+	sherlock_parameters.grad_termination_limit = 1e-7;
+	sherlock_parameters.MILP_tolerance = 5e-1;
+	sherlock_parameters.MILP_e_tolerance = 1e-10;
+	sherlock_parameters.scale_factor_for_M = 1;
+	sherlock_parameters.do_LP_certificate = true;
+	sherlock_parameters.LP_tolerance_limit = 1e-2;
+
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_12.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 12 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_13_name[] = "./network_files/neural_network_information_13" ;
+	sherlock_parameters.gradient_rate = 1e-2;
+	sherlock_parameters.grad_scaling_factor = 5e1;
+	sherlock_parameters.grad_switch_count = 1e3;
+	sherlock_parameters.grad_termination_limit = 1e-7;
+	sherlock_parameters.MILP_tolerance = 5e-1;
+	sherlock_parameters.MILP_e_tolerance = 1e-10;
+	sherlock_parameters.scale_factor_for_M = 1;
+	sherlock_parameters.LP_tolerance_limit = 1e-2;
+	sherlock_parameters.do_LP_certificate = true;
+
+	network_handler benchmark_13(benchmark_13_name);
+
+
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_13.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 13 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_14_name[] = "./network_files/neural_network_information_14" ;
+	sherlock_parameters.gradient_rate = 1e-2;
+	sherlock_parameters.grad_scaling_factor = 5e1;
+	sherlock_parameters.grad_switch_count = 1e3;
+	sherlock_parameters.grad_termination_limit = 1e-7;
+	sherlock_parameters.MILP_tolerance = 5e-1;
+	sherlock_parameters.MILP_e_tolerance = 1e-10;
+	sherlock_parameters.scale_factor_for_M = 1;
+	sherlock_parameters.do_LP_certificate = true;
+	sherlock_parameters.LP_tolerance_limit = 1e-2;
+
+	network_handler benchmark_14(benchmark_14_name);
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_14.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 14 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+
+	char benchmark_15_name[] = "./network_files/neural_network_information_15" ;
+	sherlock_parameters.gradient_rate = 1e-2;
+	sherlock_parameters.grad_scaling_factor = 5e1;
+	sherlock_parameters.grad_switch_count = 1e3;
+	sherlock_parameters.grad_termination_limit = 1e-7;
+	sherlock_parameters.MILP_tolerance = 5e-1;
+	sherlock_parameters.MILP_e_tolerance = 1e-10;
+	sherlock_parameters.scale_factor_for_M = 1;
+	sherlock_parameters.do_LP_certificate = true;
+
+	network_handler benchmark_15(benchmark_15_name);
+
+
+	input_interval.clear();
+	input_interval.resize(3);
+	input_interval[0].push_back(-0.1);input_interval[0].push_back(0.1);
+	input_interval[1].push_back(-0.1);input_interval[1].push_back(0.1);
+	input_interval[2].push_back(-0.1);input_interval[2].push_back(0.1);
+
+	create_constraint_from_interval(input_constraints, input_interval);
+
+	begin = clock();
+	benchmark_15.return_interval_output(input_constraints, output_range, 1);
+	cout << "output_range = [" << output_range[0] << " , " << output_range[1] << " ]" << endl;
+	end = clock();
+	printf("time cost for Sherlock benchmark 15 ------------------ %lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
 
   return 0;
 }
