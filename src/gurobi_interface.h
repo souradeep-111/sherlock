4b7fb79c474671fcff576e217bda1472814871af
Souradeep Dutta <souradeep.dutta@colorado.edu>
2018-02-06 11:30:57 -0700

    Update gurobi_interface.h

--- src/gurobi_interface.h
+++ src/gurobi_interface.h
@@ -1,3 +1,14 @@
+/*
+
+Contributors to the tool : 
+Souradeep Dutta
+
+email : souradeep.dutta@colorado.edu
+
+LICENSE : Please see the license file, in the main directory
+
+*/
+
 #ifndef gurobi_interface_h
 #define gurobi_interface_h
 
