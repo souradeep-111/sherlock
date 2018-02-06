7ad10d69904e6249325eb186038d85d935f5c166
Souradeep Dutta <souradeep.dutta@colorado.edu>
2018-02-06 11:30:20 -0700

    Update configuration.cpp

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -1,3 +1,14 @@
+/*
+
+Contributors to the tool : 
+Souradeep Dutta
+
+email : souradeep.dutta@colorado.edu
+
+LICENSE : Please see the license file, in the main directory
+
+*/
+
 #include "configuration.h"
 
 parameter_values sherlock_parameters;
