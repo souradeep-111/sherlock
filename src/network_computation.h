2f6be970032f9009664f2a5b5c36d6afbb3248d6
Souradeep Dutta <souradeep.dutta@colorado.edu>
2018-02-06 11:31:37 -0700

    Update network_computation.h

--- src/network_computation.h
+++ src/network_computation.h
@@ -1,3 +1,14 @@
+/*
+
+Contributors to the tool : 
+Souradeep Dutta
+
+email : souradeep.dutta@colorado.edu
+
+LICENSE : Please see the license file, in the main directory
+
+*/
+
 #ifndef network_computation_h
 #define network_computation_h
 #include <vector>
